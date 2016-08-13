ed1a2f4baaf8025dd6990ec2cd0d6421190a6d45
jondev <jdevlin6@gmail.com>
2016-08-13 16:52:01 -0400

    Updated least squares method with to loop through all rows of y

--- CoordinateDescent.py
+++ CoordinateDescent.py
@@ -103,19 +103,19 @@ class CoordinateDescent(object):
         :param ymatrix: matrix of results
         :return: a matrix of coefficients beta
         '''
-        #Initializing coefficient matrix of zeroes. Need to figure out dimensions from inputs.
+        # Initializing coefficient matrix of zeroes. Need to figure out dimensions from inputs.
         bmatrix = np.zeros()
-        #only does 3 passes through the coordinate descent algorithm. Need to make it smarter
-        for i in range(0, 3):
-            #Algorithm requires matrix with current i removed
+        # only does one pass through the coordinate descent algorithm. Need to make it loop through until convergence
+        for i in range(0, len(ymatrix)):
+            # Algorithm requires matrix with current i removed
             xtemp = scipy.delete(xmatrix, i, 1)
             btemp = scipy.delete(bmatrix, i, 0)
-            #The following line is a mess. Need to fix with numpy matrix methods
+            # The following line is a mess. Need to fix with numpy matrix methods
             numA = xmatrix.transpose()
-            numB = np.matmul(xtemp, btemp)
+            numB = np.dot(xtemp, btemp)
             numC = np.subtract(ymatrix, numB)
-            numerator = np.matmul(numA, numB)
-            denominator = np.matmul(xmatrix, numA)
+            numerator = np.dot(numA, numB)
+            denominator = np.dot(xmatrix, numA)
             bmatrix[i,0] = np.divide(numerator, denominator)
 
 def main(self):
