9f58a47c779d8207c375b67154f9604a4ead8c5f
dimitriambrazis <melenemeech@gmail.com>
2016-08-14 03:13:07 -0400

    Update CoordinateDescent.py

--- CoordinateDescent.py
+++ CoordinateDescent.py
@@ -1,76 +1,74 @@
 '''
     Dimitri Ambrazis
     Jones Devlin
-
     Final Project August 2016
 
     The Coordinate Descent class takes in an excel file, parses it, and performs coordinate descent on the data
     per prior-determined specifications.  The goal is to see what betas are produced.
-    We will then use Orange to check the betas
+    We will then use Orange to check the betas.
 
     Data Key:
-         observation[0] = id
-         observation[1] = member_id
-         observation[2] = loan_amnt
-         observation[3] = funded_amnt
-         observation[4] = funded_amnt_inv
-         observation[5] = term
-         observation[6] = int_rate
-         observation[7] = installment
-         observation[8] = grade
-         observation[9] = sub_grade
-         observation[10] = emp_title
-         observation[11] = emp_length
-         observation[12] = home_ownership
-         observation[13] = annual_inc
-         observation[14] = verification_status
-         observation[15] = issue_d
-         observation[16] = loan_status
-         observation[17] = pymnt_plan
-         observation[18] = url
-         observation[19] = desc
-         observation[20] = purpose
-         observation[21] = title
-         observation[22] = zip_code
-         observation[23] = addr_state
-         observation[24] = dti
-         observation[25] = delinq_2yrs
-         observation[26] = earliest_cr_line
-         observation[27] = inq_last_6mths
-         observation[28] = mths_since_last_delinq
-         observation[29] = mths_since_last_record
-         observation[30] = open_acc
-         observation[31] = pub_rec
-         observation[32] = revol_bal
-         observation[33] = revol_util
-         observation[34] = total_acc
-         observation[35] = initial_list_status
-         observation[36] = out_prncp
-         observation[37] = out_prncp_inv
-         observation[38] = total_pymnt
-         observation[39] = total_pymnt_inv
-         observation[40] = total_rec_prncp
-         observation[41] = total_rec_int
-         observation[42] = total_rec_late_fee
-         observation[43] = recoveries
-         observation[44] = collection_recovery_fee
-         observation[45] = last_pymnt_d
-         observation[46] = last_pymnt_amnt
-         observation[47] = next_pymnt_d
-         observation[48] = last_credit_pull_d
-         observation[49] = collections_12_mths_ex_med
-         observation[50] = mths_since_last_major_derog
-         observation[51] = policy_code
-         observation[52] = application_type
-         observation[53] = annual_inc_joint
-         observation[54] = dti_joint
-         observation[55] = verification_status_joint
-         observation[56] = acc_now_delinq
-
-
+         observations[0] = id
+         observations[1] = member_id
+         observations[2] = loan_amnt
+         observations[3] = funded_amnt
+         observations[4] = funded_amnt_inv
+         observations[5] = term
+         observations[6] = int_rate
+         observations[7] = installment
+         observations[8] = grade
+         observations[9] = sub_grade
+         observations[10] = emp_title
+         observations[11] = emp_length
+         observations[12] = home_ownership
+         observations[13] = annual_inc
+         observations[14] = verification_status
+         observations[15] = issue_d
+         observations[16] = loan_status
+         observations[17] = pymnt_plan
+         observations[18] = url
+         observations[19] = desc
+         observations[20] = purpose
+         observations[21] = title
+         observations[22] = zip_code
+         observations[23] = addr_state
+         observations[24] = dti
+         observations[25] = delinq_2yrs
+         observations[26] = earliest_cr_line
+         observations[27] = inq_last_6mths
+         observations[28] = mths_since_last_delinq
+         observations[29] = mths_since_last_record
+         observations[30] = open_acc
+         observations[31] = pub_rec
+         observations[32] = revol_bal
+         observations[33] = revol_util
+         observations[34] = total_acc
+         observations[35] = initial_list_status
+         observations[36] = out_prncp
+         observations[37] = out_prncp_inv
+         observations[38] = total_pymnt
+         observations[39] = total_pymnt_inv
+         observations[40] = total_rec_prncp
+         observations[41] = total_rec_int
+         observations[42] = total_rec_late_fee
+         observations[43] = recoveries
+         observations[44] = collection_recovery_fee
+         observations[45] = last_pymnt_d
+         observations[46] = last_pymnt_amnt
+         observations[47] = next_pymnt_d
+         observations[48] = last_credit_pull_d
+         observations[49] = collections_12_mths_ex_med
+         observations[50] = mths_since_last_major_derog
+         observations[51] = policy_code
+         observations[52] = application_type
+         observations[53] = annual_inc_joint
+         observations[54] = dti_joint
+         observations[55] = verification_status_joint
+         observations[56] = acc_now_delinq
 '''
 
 import numpy as np
+import scipy as sp
 
 from xlrd import open_workbook
 
@@ -87,7 +85,7 @@ class CoordinateDescent(object):
             number_of_columns = sheet.ncols
             items = []
             rows = []
-            for row in range(0, number_of_rows-1):
+            for row in range(0, number_of_rows):
                 for col in range(number_of_columns):
                     value = (sheet.cell(row, col).value)
                     value = str(value)
@@ -96,6 +94,38 @@ class CoordinateDescent(object):
                     observations[row][col]=value
         return observations
 
+    def homeOwnershipAnnualSalaryX(self, observations):
+        '''
+            Get x matrix.  This is composed of the home ownership and salary
+        '''
+        x = [[0 for x in range(3)] for y in range(199)]
+        for i in range(0,200):
+            if i>0:
+                x[i-1][0] = 1
+                if observations[i][12] =="RENT":
+                    x[i-1][1] = 0
+                elif observations[i][12] =="MORTGAGE":
+                    x[i-1][1] = 1
+                else:
+                    x[i - 1][1] = 2
+                x[i-1][2] = observations[i][13]
+        #for i in range(0, 199):
+            #print str(x[i][0]) + "\t" + str(x[i][1]) + "\t" + str(x[i][2])
+        return x
+
+    def findOLSBeta(self, x, y):
+        '''
+            (xT dot x)-1(xT dot y)
+        '''
+        x = np.array(x, dtype=float)
+        y = np.array(y, dtype=float)
+        xTranspose = np.transpose(x)
+        firstPart= np.linalg.inv(np.dot(xTranspose, x))
+        secondPart = np.dot(xTranspose, y)
+
+        return np.dot(firstPart, secondPart)
+
+
     def leastSquares(self, xmatrix, ymatrix):
         '''
         Reads in attributes and results to calculate coefficients using least squares
@@ -103,26 +133,36 @@ class CoordinateDescent(object):
         :param ymatrix: matrix of results
         :return: a matrix of coefficients beta
         '''
-        # Initializing coefficient matrix of zeroes. Need to figure out dimensions from inputs.
+        #Initializing coefficient matrix of zeroes. Need to figure out dimensions from inputs.
         bmatrix = np.zeros()
-        # only does one pass through the coordinate descent algorithm. Need to make it loop through until convergence
-        for i in range(0, len(ymatrix)):
-            # Algorithm requires matrix with current i removed
-            xtemp = scipy.delete(xmatrix, i, 1)
-            btemp = scipy.delete(bmatrix, i, 0)
-            # The following line is a mess. Need to fix with numpy matrix methods
+        #only does 3 passes through the coordinate descent algorithm. Need to make it smarter
+        for i in range(0, 3):
+            #Algorithm requires matrix with current i removed
+            xtemp = sp.delete(xmatrix, i, 1)
+            btemp = sp.delete(bmatrix, i, 0)
+            #The following line is a mess. Need to fix with numpy matrix methods
             numA = xmatrix.transpose()
-            numB = np.dot(xtemp, btemp)
+            numB = np.matmul(xtemp, btemp)
             numC = np.subtract(ymatrix, numB)
-            numerator = np.dot(numA, numB)
-            denominator = np.dot(xmatrix, numA)
+            numerator = np.matmul(numA, numB)
+            denominator = np.matmul(xmatrix, numA)
             bmatrix[i,0] = np.divide(numerator, denominator)
 
 def main(self):
+    yValues=[0 for x in range(199)]
     cd = CoordinateDescent()
     observations=cd.readData('Loan.xlsx')
-    print(observations[0])
+
+    # Case 1: Does home ownership and salary affect the loan grade?
+    xValues=cd.homeOwnershipAnnualSalaryX(observations)
+
+    for i in range(0,200):
+        if i>0:
+            yValues[i-1]=float(observations[i][6])
+    betas = cd.findOLSBeta(xValues, yValues)
+    for i in range(len(betas)):
+        print(betas[i])
 
 
 if __name__ == '__main__':
-     main(object)
+    main(object)
